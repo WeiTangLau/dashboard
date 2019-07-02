3118a9542662a378307f9cb7ff56a59a3a4ffd63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-02 06:55:11 +0000

    Deploy rajtang/dashboard to github.com/rajtang/dashboard.git:gh-pages

---
+++ precache-manifest.3bef6b8c7b7928e36f5ffc8db242a217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d42da47ef2751383328e1da654f4360a",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "0e1ef41b9340cc6a2e44",
+    "url": "/dashboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/dashboard/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0e1ef41b9340cc6a2e44",
+    "url": "/dashboard/static/js/main.f69f4102.chunk.js"
+  },
+  {
+    "revision": "dcfbc05562333eb34d98",
+    "url": "/dashboard/static/js/runtime~main.26f3d777.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
